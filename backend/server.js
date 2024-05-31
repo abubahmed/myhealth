683122cdd42ca5a64f495fef4367a87c309c1b20
adb8 <abuahmed0821@gmail.com>
2024-05-31 09:38:36 -0400

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 const app = express();
-const port = 5000;
 app.use(cors());
 app.use(express.json());
 
+const DB_NAME = process.env.DB_NAME || "myhealth"
+const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { dbName: "myhealth", useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { dbName: DB_NAME, useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
@@ -27,6 +28,6 @@ app.use("/appointments", appointmentsRouter);
 const messagesRouter = require("./routes/messages.route.js");
 app.use("/messages", messagesRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
